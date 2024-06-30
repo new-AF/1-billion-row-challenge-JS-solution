5b3d3653a72d07bae3525628bf833cafb63a550f
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2024-06-30 08:13:48 +0200

    add line stream reading and limiting

--- my solution.js
+++ my solution.js
@@ -1,3 +1,26 @@
 const log = console.log;
+const fs = require("fs");
+const readline = require("readline");
 
-log("hello");
+// log("hello");
+const originalStream = fs.createReadStream("./weather_stations.csv", {
+    encoding: "utf8",
+});
+const stream = readline.createInterface({
+    input: originalStream,
+    crlfDelay: Infinity,
+});
+
+const stations = new Map();
+let count = 0;
+
+const onLine = (chunk) => {
+    log(chunk, count);
+    ++count;
+    if (count > 5) {
+        stream.close();
+        stream.removeAllListeners();
+    }
+};
+stream.on("line", onLine);
+// stream.on("close", onLine);
