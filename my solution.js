984e01d690586020fd3b3213226240386dfc576b
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2024-06-30 09:30:39 +0200

    add initial solution and bugfix line stream limiting

--- my solution.js
+++ my solution.js
@@ -1,4 +1,10 @@
-const log = console.log;
+const Do_Log = false;
+const Max_Lines_To_Print = Infinity;
+
+const log = (...data) => {
+    if (Do_Log === false) return;
+    console.log(data);
+};
 const fs = require("fs");
 const readline = require("readline");
 
@@ -19,17 +25,57 @@ const processLine = (line) => {
 
     const [city, temperatureString] = line.split(";");
     const temperature = parseFloat(temperatureString);
-    // log("--", city, temperature);
+    log("--", city, temperature);
+
+    if (stations.has(city) === false)
+        stations.set(
+            city,
+            new Map([
+                ["name", city],
+                ["min", Infinity],
+                ["max", -Infinity],
+                ["sum", 0],
+                ["count", 0],
+                ["average", undefined],
+            ])
+        );
+
+    const map = stations.get(city);
+    const min = map.get("min");
+    const max = map.get("max");
+    const sum = map.get("sum");
+    const count = map.get("count");
+
+    map.set("min", temperature < min ? temperature : min);
+    map.set("max", temperature > max ? temperature : max);
+    map.set("sum", sum + temperature);
+    map.set("count", count + 1);
 };
 
 const onLine = (line) => {
     ++count;
-    log(line, count);
-    if (count >= 5) {
+    log(count, line);
+    processLine(line);
+
+    if (count >= Max_Lines_To_Print) {
         stream.close();
         stream.removeAllListeners();
     }
-    processLine(line);
+};
+const onClose = (event) => {
+    // log(stations);
+    log(`
+// results //
+`);
+
+    stations.forEach((map, city) => {
+        const average = map.get("sum") / map.get("count");
+        const min = map.get("min");
+        const max = map.get("max");
+        const count = map.get("count");
+        // if (count === 1) return;
+        console.log(`${city}:${min},${average},${max},${count}`);
+    });
 };
 stream.on("line", onLine);
-// stream.on("close", onLine);
+stream.on("close", onClose);
